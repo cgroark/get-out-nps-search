559f525b27eb3e4c2ba8c1e9b0b11b66554e3685
cgroark <colingroark@gmail.com>
2018-01-13 17:48:41 -0800

    main styling done

--- public/js/script_state.js
+++ public/js/script_state.js
@@ -1,19 +1,22 @@
-  
-  var latLong = dataObject.parkStuff.Latlong;
-  var newLat = latLong.split(":");
+  for(var i=0; i<latLongData.length; i++){
+  var newLat = latLongData[i].latLong.split(":");
   var longitude = parseFloat(newLat[2]);
   var lattitude = parseFloat(newLat[1].split(",")[0]);
   var locations = [];
   locations.push({lat:lattitude, lng:longitude});
   console.log(locations);
-  
+
+  }
   function initMap() {
-        var map = new google.maps.Map(document.getElementById("state-map"), {
+    
+        var map = new google.maps.Map(document.getElementById("state_all_map"), {
           center: locations[0],
           zoom: 11
         });
 
+        // Create a marker and set its position.
         var marker, i;
+        
         for (i = 0; i < locations.length; i++) {  
           marker = new google.maps.Marker({
           position: new google.maps.LatLng(locations[i]),
@@ -21,6 +24,8 @@
       });
       };
     }
+
+
 
 
 
